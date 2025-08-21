9629dc7da58c6f115b1285bfa4f4520536dee217
Kamil Baraniak <90936580+kbaraniak@users.noreply.github.com>
2025-08-21 19:26:22 +0200

    Archived from local disk

---
+++ main.js
@@ -0,0 +1,8 @@
+const home = "http://puri.zone"
+const gt = "https://github.com/Puri-Devs"
+const ln = "https://www.linkedin.com/in/puri-developers-83a732240/"
+const yt = "https://youtube.com/@klubuntu"
+
+function op(url){
+    window.open(url)
+}
